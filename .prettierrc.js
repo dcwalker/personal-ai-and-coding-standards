1f3b262ff878d16b6d2aca32763d1f0f4bb53bdf
Dan Walker <dan.walker@gmail.com>
2025-11-17 12:23:47 -0800

    Add Prettier configuration and pre-commit hook for markdown formatting

---
+++ .prettierrc.js
@@ -0,0 +1,16 @@
+// Local prettier config to replace the missing @procore/core-prettier dependency
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  trailingComma: 'all',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: 'always',
+  endOfLine: 'lf',
+  jsxSingleQuote: true,
+};
+
